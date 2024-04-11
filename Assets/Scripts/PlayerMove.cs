16b75e848c77f90ea393c0a3bb0b594f1e8d274e
iinabe <irinamellow@gmail.com>
2024-04-11 15:10:16 +0300

    move ship
    движение корабля (но он медленный)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,45 +1,19 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
 
-    //переменная для позиции клика
-    Vector3 clickPos;
-    //переменная для вектора движения
-    Vector3 move;
-    //переменная для скорости движения
-    public float speed = 1;
-    //переменная для ссылки на Rigidbody2D
-    Rigidbody2D rb;
-
-    //выполнится один раз при старте скрипта
-    void Start()
-    {
-        //делаем ссылку на компонент Rigidbody2D
-        rb = GetComponent<Rigidbody2D>();
-        //что-бы корабль остался на месте и не полетел к точке (0,0,0)
-        clickPos = transform.position;
-    }
-
-    //исполняется каждый новый кадр
     void Update()
     {
-        //проверка, нажата-ли левая кнопка мыши
-        if (Input.GetMouseButton(0))
-        {
-            //трансформировать координаты курсора в координаты игрового мира
-            clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
-        //вычислить новый вектор движения
-        move = clickPos - transform.position;
-    }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-    //выполняется через определенные периоды (0.02 по умолчанию). Используйте для вычислений физики
-    void FixedUpdate()
-    {
-        //измените вектор движения
-        //z останется 0, что-бы корабль не двигался по z-оси
-        rb.velocity = new Vector2(move.x, move.y) * speed;
+        float horizontalMovement = horizontalInput * speed * Time.deltaTime;
+        float verticalMovement = verticalInput * speed * Time.deltaTime;
+
+        transform.Translate(new Vector3(horizontalMovement, verticalMovement, 0));
     }
 }
