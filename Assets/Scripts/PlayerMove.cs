f7d779742d8c128a957b7a9bab511270873b3093
iinabe <irinamellow@gmail.com>
2024-04-13 12:16:56 +0300

    джойстик большой

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public Joystick joystick;
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = joystick.Horizontal;
+        float verticalInput = joystick.Vertical;
 
         float horizontalMovement = horizontalInput * speed * Time.deltaTime;
         float verticalMovement = verticalInput * speed * Time.deltaTime;
