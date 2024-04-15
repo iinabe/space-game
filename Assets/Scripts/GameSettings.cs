90c03850e3f256388e232340ad4f878dcd2427f4
sinpiks <daria.diklova@yandex.ru>
2024-04-15 16:22:35 +0300

    новый скрипт со структурами

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    struct SceneNames
+    {
+        const string Menu = "Menu";
+        const string MainScene = "MainScene";
+    }
+}
+
