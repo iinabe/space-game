4f5cc74425d9897d0a4c271a9978e1866e56f87e
mylootbae <sedashova44@gmail.com>
2024-05-16 01:37:52 +0300

    armor script

--- Assets/Scripts/GameSettings/HpController.cs
+++ Assets/Scripts/GameSettings/HpController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System.Collections;
 
 public class HpController : MonoBehaviour
 {
@@ -7,14 +6,16 @@ public class HpController : MonoBehaviour
 
     public ObjectType objectType;
 
-
     private int hp;
-    private int maxHp = 100; 
-    public AudioClip explosionSound; 
-    private AudioSource audioSource; 
+    private int maxHp = 100;
+    private int armor;
+    private int maxArmor = 100;
+    public AudioClip explosionSound;
+    private AudioSource audioSource;
 
     public GameObject Explosion;
     public HealthBar healthBar;
+    public ArmorBar armorBar;
 
     void Start()
     {
@@ -22,10 +23,17 @@ public class HpController : MonoBehaviour
         {
             case ObjectType.Player:
                 hp = maxHp;
-               if (healthBar != null)
-        {
-            healthBar.SetMaxHealth(maxHp);
-        }
+                armor = maxArmor;
+                if (healthBar != null)
+                {
+                    healthBar.SetMaxHealth(maxHp);
+                    healthBar.SetHealth(hp);
+                }
+                if (armorBar != null)
+                {
+                    armorBar.SetMaxArmor(maxArmor);
+                    armorBar.SetArmor(armor);
+                }
                 break;
             case ObjectType.Asteroid:
                 hp = 3;
@@ -37,43 +45,66 @@ public class HpController : MonoBehaviour
                 hp = maxHp;
                 break;
         }
-        audioSource = GetComponent<AudioSource>(); 
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void MakeDamage(int damage)
     {
-        hp -= damage;
+        // Если есть броня, урон сначала уменьшает броню
+        if (armor > 0)
+        {
+            int armorDamage = Mathf.Min(damage, armor);
+            armor -= armorDamage;
+            damage -= armorDamage;
 
+            // Обновляем полоску брони
+            if (armorBar != null)
+            {
+                armorBar.SetArmor(armor);
+            }
+        }
+
+        // Если урон остаётся после уменьшения брони, он уменьшает здоровье
+        if (damage > 0)
+        {
+            hp -= damage;
+        }
+
+        // Обновляем полоску здоровья
+        if (objectType == ObjectType.Player && healthBar != null)
+        {
+            healthBar.SetHealth(hp);
+        }
+
+        // Проверяем, умер ли объект
         if (hp <= 0)
-        {   
+        {
             AudioSource.PlayClipAtPoint(explosionSound, this.gameObject.transform.position);
             if (objectType == ObjectType.Turret)
             {
                 GetComponent<turretScript>().HpLost();
             }
             Instantiate(Explosion, transform.position, Quaternion.identity);
-            
             Destroy(gameObject);
-            
         }
-        else if (objectType == ObjectType.Player && healthBar != null)
-            {
-                healthBar.SetHealth(hp);
-            }
     }
 
     public void Heal(int healAmount)
     {
         hp += healAmount;
-        Debug.Log("Health increased by " + healAmount + ", current health is " + hp);
-        hp = Mathf.Min(hp, maxHp); 
+        hp = Mathf.Min(hp, maxHp);
+        if (healthBar != null)
+        {
+            healthBar.SetHealth(hp);
+        }
+        Debug.Log("Здоровье увеличено на " + healAmount + ", текущее здоровье: " + hp);
     }
 
     void PlayExplosionSound()
     {
         if (explosionSound != null && audioSource != null)
         {
-            audioSource.PlayOneShot(explosionSound); 
+            audioSource.PlayOneShot(explosionSound);
         }
     }
 }
